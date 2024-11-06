11514b0027071c92a5ed5ba87c9ae56902b59556
utkarsh-deuglo <utkarsh.deuglo253@gmail.com>
2024-11-06 23:22:51 +0530

    stack icon and mini bio changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import { HashLink as Link } from 'react-router-hash-link';
 import { Box } from "@mui/material";
 import { info } from "../info/Info";
 import { singlePage } from '../info/Info';
+import stackImg from '../img/stack.png';
 
 const links = [
     {
@@ -14,12 +15,13 @@ const links = [
         active: 'home'
     },
     {
-        name: 'About Me',
+        name: 'About_Me',
         to: 'about',
         active: 'about'
     },
     {
-        name: info.initials,
+        // name: info.initials,
+        name: stackImg,
         type: 'initials',
         to: 'tech-stack',
         active: 'tech-stack'
@@ -35,15 +37,15 @@ const links = [
 // when you click on the nav buttons to scroll down.
 const scrollWidthOffset = (el) => {
     const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
-    const yOffset = -80; 
-    window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' }); 
+    const yOffset = -80;
+    window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' });
 }
 
 
 export default function Navbar({ darkMode, handleClick, active, setActive }) {
 
     return (
-        <Box component={'nav'} width={'100%'} position={singlePage ? 'fixed' : 'relative'} className={darkMode? Style.dark : Style.light}>
+        <Box component={'nav'} width={'100%'} position={singlePage ? 'fixed' : 'relative'} className={darkMode ? Style.dark : Style.light}>
             <Box component={'ul'} display={'flex'} justifyContent={'center'} alignItems={'center'}
                 gap={{ xs: '2rem', md: '8rem' }}
                 textTransform={'lowercase'} fontSize={'1rem'}>
@@ -51,11 +53,12 @@ export default function Navbar({ darkMode, handleClick, active, setActive }) {
                     <Box key={index} component={'li'} className={(link.active === active && !link.type) && Style.active}
                         sx={{ borderImageSource: info.gradient }}>
                         <Link to={singlePage ? `#${link.to}` : `/${link.to}`}
-                        scroll={el => scrollWidthOffset(el)}
+                            scroll={el => scrollWidthOffset(el)}
                             smooth
                             onClick={() => setActive(link.active)} className={Style.link}>
                             {!link.type && <p style={{ padding: '0.5rem 0' }}>{link.name}</p>}
-                            {link.type && <h1>{link.name}</h1>}
+                            {/* {link.type && <h1 style={{ textTransform: 'uppercase' }}>{link.name}</h1>} */}
+                            {link.type && <img src={link.name} width={40} alt={link.name} />}
                         </Link>
                     </Box>
                 ))}
@@ -63,6 +66,6 @@ export default function Navbar({ darkMode, handleClick, active, setActive }) {
                     <Toggler darkMode={darkMode} handleClick={handleClick} />
                 </li>
             </Box>
-        </Box>
+        </Box >
     )
 }
