a12d7d8776e4b21710ba980d26d609e96bdb09bc
utkarshgangwar <utkarshgangwar22@gmail.com>
2025-02-06 20:02:13 +0530

    fixes

--- src/info/Info.js
+++ src/info/Info.js
@@ -44,7 +44,7 @@ export const info = {
         },
         {
             "emoji": "💼",
-            "text": "Fullstack Developer"
+            "text": "MERN Stack Developer"
         },
         {
             "emoji": "🧠",
